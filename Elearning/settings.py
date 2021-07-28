f60e873139c5bb45f4915d9b94c46f3cb9dd97c8
Aayush Kumar Singh <malemeaayushkumarsingh@gmail.com>
2021-07-28 10:42:25 +0530

    Update settings.py

--- Elearning/settings.py
+++ Elearning/settings.py
@@ -132,9 +132,9 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 MEDIA_URL = "/media/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+#STATICFILES_DIRS = [
+#    BASE_DIR / "static",
+#]
 
 
 
